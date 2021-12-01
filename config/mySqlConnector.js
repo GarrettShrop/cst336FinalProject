747cdc0181942b0b6c853aa516be7a4ecdd06224
Virus <gshropshire@csumb.edu>
2021-12-01 12:59:27 -0800

    working still on db connection

--- config/mySqlConnector.js
+++ config/mySqlConnector.js
@@ -5,13 +5,13 @@ const {DBHost, DBUser, DBPassword, DBDatabase} = require('./config');
 // const connection = mysql.createConnection(process.env.'mysql://jox7vgipufqsc9yd:yuea4xnzkocghi8v@x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/rwl3mosgwuhr2n4s')
 // connection.connect();
 
-// let pool = mysql.createPool({
-//     connectionLimit : 10,
-//     host : DBHost,
-//     username : DBUser,
-//     password : DBPassword,
-//     database : DBDatabase
-// });
+let pool = mysql.createPool({
+    connectionLimit : 10,
+    host : DBHost,
+    username : DBUser,
+    password : DBPassword,
+    database : DBDatabase
+});
 
 pool.query = util.promisify(pool.query);
 
